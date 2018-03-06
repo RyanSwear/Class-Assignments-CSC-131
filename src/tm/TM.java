416db0fb5980c05281d3fda2eeac85a4c13e5399
Ryan Swearingen <ryanmswearingen@gmail.com>
2018-03-06 13:35:25 -0800

    implemented changes to summary method

--- src/tm/TM.java
+++ src/tm/TM.java
@@ -15,226 +15,112 @@ public class TM {
     tm.run(args);
     }
     void run(String[] args)
-  {
-    if(args.length == 0)
     {
-      System.out.println("Please enter execute program with the following format:  JAVA TM <cmd> <task> <data>");
-      return;
-    }
-    else if ( args.length > 0)
-    {
-        String cmd = args[0].toUpperCase();
-        LinkedList<String> fileList = new LinkedList();//will load the entire file line by line to a string, the string will then be split into tokens for further disection
-        LinkedList<Task> taskList = new LinkedList();//used to store each task with its appropriate information
-    //check the command entered and execute the appropriate method
-        switch (cmd)
+        if(args.length == 0)
         {
-        case "START":
+            System.out.println("Please enter execute program with the following format:  JAVA TM <cmd> <task> <data>");
+            return;
+        }
+        else if ( args.length > 0)
+        {
+            String cmd = args[0].toUpperCase();
+       
+            //check the command entered and execute the appropriate method
+            switch (cmd)
             {
-            String data = args[1];
-            tmModel.startTask(data);
-            break;
-            }
-        case "STOP":
-            {
-            String data = args[1];
-            tmModel.stopTask(data);
-            break;
-            }
-        case "SUMMARY":
-            {
-             String taskToSum = new String();// will be used to determine if user wants to summarize a particular task or not
-             if (args.length == 1)
-             {
-                taskToSum = " ";
-             }
-             else{taskToSum = args[1];}
-             
-            this.summary(fileList, taskList, taskToSum);
-            break;
-            }
-        case "DESCRIBE":
-            {
-                if (args.length == 4)
+            case "START":
                 {
-                    String data = args[1];
-                    String description = args[2];
-                    String size = args[3];
-                    tmModel.describeTask(data,description);
-                    tmModel.sizeTask(data, size);
+                String data = args[1];
+                tmModel.startTask(data);
+                break;
+                }
+            case "STOP":
+                {
+                String data = args[1];
+                tmModel.stopTask(data);
+                break;
+                }
+            case "SUMMARY":
+                {
+                String taskToSum = new String();// will be used to determine if user wants to summarize a particular task or not
+                if (args.length == 1)
+                {
+                    this.summary();
                 }
                 else
                 {
-                   String data = args[1];
-                   String description = args[2];
-                   tmModel.describeTask(data,description);
+                    taskToSum = args[1];
+                    this.summary(taskToSum);
                 }
-            break;
-            }
-        case "SIZE":
-            {
-                String data = args[1];
-                String size = args[2];
-                tmModel.sizeTask(data,size);
+                break;
+                }
+            case "DESCRIBE":
+                {
+                    if (args.length == 4)
+                    {
+                        String data = args[1];
+                        String description = args[2];
+                        String size = args[3];
+                        tmModel.describeTask(data,description);
+                        tmModel.sizeTask(data, size);
+                    }
+                    else
+                    {
+                        String data = args[1];
+                        String description = args[2];
+                        tmModel.describeTask(data,description);
+                    }
                 break;
             }
-        default:
-        {
-            System.out.println("Please enter a valid command: start, stop, describe, size, summary");
-            break;
-        }
-    }
-    }
-    
-  }
- 
-  void summary(LinkedList<String> fList, LinkedList<Task> tList, String taskToSum)
-  {
-    Log log = new Log();
-    String line = new String();
-    int i = 0;
-    log.read(fList);// start reading at the first line of the text file
-    while (i < fList.size())
-    {
-        line = fList.get(i);// analyze each line
-        String[] tokens = line.split(" ",3);// splits into cmd task data
-        
-        //add each task to task list
-        if (tList.isEmpty())
-        {
-            Task task = new Task();
-            task.name = tokens[1];
-            switch (tokens[0])
-            {//check the command that was entered on each line
-                //enter data into the task depending on the command
-                case "Start":
-                        Long ts = Long.parseLong(tokens[2]);
-                        task.starTime.addFirst(ts);
-                        break;
-                case "Stop":
-                        task.endTime.addFirst(Long.parseLong(tokens[2]));
-                        break;
-                case "Describe": 
-                        task.description.addFirst(tokens[2]);
-                        break;
-                case "Size":
-                        task.size = tokens[2];
-                        break;
-                default:
+            case "SIZE":
+                {
+                    String data = args[1];
+                    String size = args[2];
+                    tmModel.sizeTask(data,size);
                     break;
-            }
-                
-            tList.add(task);
-        }
-        else
-        {
-            //check if task has already been inserted, check for its name
-            Task task = new Task();
-            int j = 0;
-            boolean taskFound = false;
-            while ( (j < tList.size()) && (taskFound == false))
-            {
-                task = tList.pollLast();
-                if (task.name.equals(tokens[1]))
-                {
-                    switch (tokens[0])
-                    {//check the command that was entered on each line
-                //enter data into the task depending on the command
-                        case "Start":
-                            task.starTime.addLast(Long.parseLong(tokens[2]));
-                            break;
-                        case "Stop":
-                            task.endTime.addLast(Long.parseLong(tokens[2]));
-                            break;
-                        case "Describe": 
-                            task.description.addLast(tokens[2]);
-                            break;
-                        case "Size":
-                            task.size = tokens[2];
-                            break;
-                        default:
-                            break;
-                    }
-                    tList.push(task);
-                    taskFound = true;
                 }
-                else
-                    tList.push(task);
-                j++;
-            }
-            if (taskFound == false)//task was not found in the list, add a new entry
+            default:
             {
-                 Task task2 = new Task();
-                 task2.name = tokens[1];
-                 switch (tokens[0])
-                {
-                    case "Start":
-                        task2.starTime.addFirst(Long.parseLong(tokens[2]));
-                        break;
-                    case "Stop":
-                        task2.endTime.addFirst(Long.parseLong(tokens[2]));
-                        break;
-                    case "Describe": 
-                        task2.description.addFirst(tokens[2]);
-                        break;
-                    case "Size":
-                        task2.size = tokens[2];
-                        break;
-                    default:
-                        break;
-                }
-                
-                tList.push(task2);
+                System.out.println("Please enter a valid command: start, stop, describe, size, summary");
+                break;
             }
-        }
-        i++;
-    }
-    
-    //check if all tasks or just a certain task should be displayed
-    if (taskToSum == " ")
-    {//if no task to check was entered, display all
-        int k = 0;
-        int h = tList.size();
-        while (k < h)
-        {
-            Task display = new Task();
-            display = tList.pop();
-            System.out.println(display.name + ": Total Time HH MM SS:  " + display.getTotalTimeString() + " Size: " + display.size);
-            display.printDes();
-            System.out.println();
-            k++;
-        }
-    }
-    else
-    {
-        int k = -1;
-        boolean taskFound = false;
-        while (k < tList.size())
-        {
-            Task display = new Task();
-            display = tList.pop();
-            if (display.name.equals(taskToSum))
-            {
-                //check if the current task is the one the user wants
-                // if so display its summary
-                taskFound = true;
-                System.out.println(display.name + ": Total Time HH MM SS:  " + display.getTotalTime() + " Size: " + display.size);
-                display.printDes();
-                System.out.println("\n");
             }
-            k++;
-        }
-        if (!taskFound)
-        {
-            //if the task the user wants is not in the text file
-            //tell the user it is not in the file
-            System.out.println("The wanted task cannot be found.");
-        }
-    }
+        }   
     
+    }
+ 
+  void summary ()
+  {
+      Set<String> x = tmModel.taskNames();
+      String[] tasks = x.toArray(new String[x.size()]);
+      //display each task and its characteristics
+      for (int i = 0; i < tasks.length; i++)
+      {
+          System.out.println("Task Name: " + tasks[i] + " Total Time:" + tmModel.taskElapsedTime(tasks[i]));
+          System.out.println("Description: " + tmModel.taskDescription(tasks[i]));
+          System.out.println("Size: " + tmModel.taskSize(tasks[i]));
+          System.out.println();
+      }
+      //display time statistics
+      Set<String> y = tmModel.taskSizes();
+      String[] sizes = y.toArray(new String[x.size()]);
+      for (int i = 0; i < sizes.length; i++)
+      {
+          System.out.println("Size: " + sizes[i] + " Min Time: " + tmModel.minTimeForSize(sizes[i]) + " seconds  Max Time: " + tmModel.maxTimeForSize(sizes[i]) + " seconds");
+          System.out.println("Average Time: " + tmModel.avgTimeForSize(sizes[i]) + " seconds " + "\n");
+          Set<String> tasksWSize = tmModel.taskNamesForSize(sizes[i]);
+          System.out.println("Tasks with this size: " + tasksWSize + "\n");
+      }
+      //display total time
+      System.out.println("Total Time For All Tasks: " + tmModel.elapsedTimeForAllTasks() + " seconds");
   }
-  
+  void summary(String taskToSum)
+  {
+          System.out.println("Task Name: " + taskToSum + " Total Time:" + tmModel.taskElapsedTime(taskToSum));
+          System.out.println("Description: " + tmModel.taskDescription(taskToSum));
+          System.out.println("Size: " + tmModel.taskSize(taskToSum));
+          System.out.println();
+  }
+
 }
-
-
 
